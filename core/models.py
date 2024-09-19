12f94c765352469adc67e56dcf42da3f2ce98500
Wagner Sena <wagnersena@hotmail.com>
2024-09-19 19:13:08 -0300

    Reorganização da estrutura do projeto - Complemento core/models.py

---
+++ core/models.py
@@ -0,0 +1,5 @@
+from .clientes import Cliente, ClientePessoaFisica, Filial
+from equipamentos.models import Equipamento
+from .contratos import Contrato
+from .ordens_servico import OrdemServico, Manutencao
+from .faturamento import Faturamento
