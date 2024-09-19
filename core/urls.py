def58cf13f3ec30f4977262e9e3e2319a429c57b
Wagner Sena <wagnersena@hotmail.com>
2024-09-19 11:55:42 -0300

    Criado urls.py principal e atualizado urls.py dos apps

---
+++ core/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    # Adicione suas URLs específicas do core aqui
+]
---
+++ equipamentos/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    # Adicione suas URLs específicas de equipamentos aqui
+]
